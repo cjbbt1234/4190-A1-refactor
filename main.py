ce868de13cd8d12be5b780e178bbc034e1dc84dd
Siyuan Shang <cjbbt12@gmail.com>
2021-02-21 21:34:37 -0700

    main file comment

--- main.py
+++ main.py
@@ -13,68 +13,114 @@ LIMIT = 2
 
 
 def back_track_std(solution, blocks, length):
+    """
+    Function caller
+    :param solution: the star solution list (empty here)
+    :param blocks: the list contain all blocks
+    :param length: the side length of the puzzle
+    :return: the iteration counter on recursion
+    """
     back_track.counter = 0
     back_track(solution, 0, blocks, length)
     return back_track.counter
 
 
 def back_track_h1(solution, blocks, length):
+    """
+    Function caller
+    """
     back_track_heuristic_one.counter = 0
     back_track_heuristic_one(solution, 0, blocks, length)
     return back_track_heuristic_one.counter
 
 
 def back_track_h2(solution, blocks, length):
+    """
+    Function caller
+    """
     back_track_heuristic_two.counter = 0
     back_track_heuristic_two(solution, 0, blocks, length)
     return back_track_heuristic_two.counter
 
 
 def back_track_hybrid(solution, blocks, length):
+    """
+    Function caller
+    """
     back_track_heuristic_hybrid.counter = 0
     back_track_heuristic_hybrid(solution, 0, blocks, length)
     return back_track_heuristic_hybrid.counter
 
 
 def forward_check_std(solution, blocks, length):
+    """
+    Function caller
+    """
     forward_checking.counter = 0
     forward_checking(solution, 0, blocks, length)
     return forward_checking.counter
 
 
 def forward_check_h1(solution, blocks, length):
+    """
+    Function caller
+    """
     forward_checking_heuristic_one.counter = 0
     forward_checking_heuristic_one(solution, 0, blocks, length)
     return forward_checking_heuristic_one.counter
 
 
 def forward_check_h2(solution, blocks, length):
+    """
+    Function caller
+    """
     forward_checking_heuristic_two.counter = 0
     forward_checking_heuristic_two(solution, 0, blocks, length)
     return forward_checking_heuristic_two.counter
 
 
 def forward_check_hybrid(solution, blocks, length):
+    """
+    Function caller
+    """
     forward_checking_heuristic_hybrid.counter = 0
     forward_checking_heuristic_hybrid(solution, 0, blocks, length)
     return forward_checking_heuristic_hybrid.counter
 
 
 def solve_puzzle(length, blocks, fnc):
+    """
+    solve the puzzle use fnc function
+
+    :param length: the side length of the puzzle
+    :param blocks: the list contain all blocks
+    :param fnc: the function name will ues in solve the puzzle
+    :return:
+    """
     solution = StarList(length * 2)
-    start = timeit.default_timer()
-    iteration = fnc(solution, blocks, length)
+    start = timeit.default_timer()  # set up the timer
+    iteration = fnc(solution, blocks, length)  # solve the puzzle use fnc, get the iteration
     stop = timeit.default_timer()
     print(fnc.__name__, ': Time cost: ', stop - start, 'second')
-    print('Have ', iteration, 'iterations')
+    print('Have', iteration, 'iterations')
     print('Solution is:', solution.get_solution_list())
     print()
     print()
     draw_solution(blocks, solution.get_solution_list())
-    return
+    # Draw the puzzle and solution in UI, please remember the time you watch the result will also count in total
+    # running time of the process. This will case abort from this process when the UI display time reach 10 minute.
+    # But the display time will not count in recursion time. When you close the UI, it will return and go for next.
 
 
 def process_solve_puzzle(length, blocks, fnc):
+    """
+    make a process to count the time of solving puzzle, and when it is timeout kill it
+
+    :param length: the side length of the puzzle
+    :param blocks: the list contain all blocks
+    :param fnc: the function name will ues in solve the puzzle
+    :return:
+    """
     process = multiprocessing.Process(target=solve_puzzle, args=(length, blocks, fnc,))
 
     process.start()
@@ -89,6 +135,9 @@ def process_solve_puzzle(length, blocks, fnc):
         process.kill()
         process.join()
 
+
+# main
+#######################################################################################################################
 
 if __name__ == '__main__':
     file_name_1 = "grid8x8.txt"
